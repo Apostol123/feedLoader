c9eadda702ba814747c2e7b849248a67d4aaf4ab
Alex Apostol <apostol516@gmail.com>
2023-07-25 16:43:23 +0200

    Extract `FeedCacheSpy` into a share scope to avoid duplication

---
+++ FeedLoaderTests/FeedCache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,49 @@
+//
+//  FeedStoreSpy.swift
+//  FeedLoaderTests
+//
+//  Created by alexandru.apostol on 25/7/23.
+//
+
+import Foundation
+import FeedLoader
+
+class FeedStoreSpy: FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    private var deletionCompletion = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+
+    enum RecivedMessages: Equatable {
+        case deletedCachedFeed
+        case insert([LocalFeedImage], Date)
+    }
+
+    private(set) var recivedMessages = [RecivedMessages]()
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        deletionCompletion.append(completion)
+        recivedMessages.append(.deletedCachedFeed)
+    }
+
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletion[index](error)
+    }
+
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletion[index](nil)
+    }
+
+    func insert(_ items: [LocalFeedImage], timeStamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        recivedMessages.append(.insert(items, timeStamp))
+    }
+
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
+    }
+
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+}
---
+++ FeedLoaderTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,26 @@
+//
+//  LoadFeedFromCacheUseCaseTests.swift
+//  FeedLoaderTests
+//
+//  Created by alexandru.apostol on 23/7/23.
+//
+
+import XCTest
+import FeedLoader
+
+final class LoadFeedFromCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        XCTAssertEqual(store.recivedMessages, [])
+    }
+
+    // MARK: - Helpers
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
