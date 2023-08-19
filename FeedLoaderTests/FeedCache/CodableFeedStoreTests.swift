a735a8f5a350df3362fa198c6ddf9e367abe5df1
Apostol123 <apostol516@gmail.com>
2023-08-19 09:58:22 +0200

    Make the storeURL explicit within the test to facilitate debuggin if this test ever fails (all relevant etails within a test method should be cleary visible))

--- FeedLoaderTests/FeedCache/CodableFeedStoreTests.swift
+++ FeedLoaderTests/FeedCache/CodableFeedStoreTests.swift
@@ -95,9 +95,10 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_deliversFailureOnRetrievalError() {
-        let sut = makeSUT()
+        let storeURL = testSpecificStoreURL()
+        let sut = makeSUT(storeURL: storeURL)
         
-        try! "invalidData".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+        try! "invalidData".write(to: storeURL, atomically: false, encoding: .utf8)
         
         expect(sut, toRetrieve: .failure(anyError()))
     }
@@ -111,9 +112,8 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     //MARK: Helpers
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
-        let storeURL = testSpecificStoreURL()
-        let sut = CodableFeedStore(storeURL: storeURL)
+    private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
+        let sut = CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
