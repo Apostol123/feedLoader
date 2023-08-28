e5387ab934229716a3e24220c256a25f961af86f
Apostol123 <apostol516@gmail.com>
2023-08-28 07:17:59 +0200

    insert inital tests

--- FeedLoaderTests/FeedCache/FeedLoaderCoreDataTests.swift
+++ FeedLoaderTests/FeedCache/FeedLoaderCoreDataTests.swift
@@ -31,11 +31,15 @@ final class FeedLoaderCoreDataTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_insert_deliversNoErrorOnEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
     }
     
     func test_insert_deliversNoErrorOnNonEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
     func test_insert_overridesPreviouslyInsertedCacheValues() {
