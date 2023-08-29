599db9e6d99c6ba4cb5b14e1b8e89ade2dd257ba
Apostol123 <apostol516@gmail.com>
2023-08-29 06:41:17 +0200

    Clean up & undo All Cache side-effects on `setUp` and `tearDown` to avoid sharing state between tests

--- FeedLoaderCacheIntegrationTests/FeedLoaderCacheIntegrationTests.swift
+++ FeedLoaderCacheIntegrationTests/FeedLoaderCacheIntegrationTests.swift
@@ -9,6 +9,17 @@ import XCTest
 import FeedLoader
 
 final class FeedLoaderCacheIntegrationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
 
@@ -26,6 +37,33 @@ final class FeedLoaderCacheIntegrationTests: XCTestCase {
 
         }
         wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_load_deliversItemsSavedOnASeparateInstance() {
+        let sutToPerfomeSave = makeSUT()
+        let sutToPerformLoad =  makeSUT()
+        let feed = uniqueImageFeed().models
+        
+        let saveExp = expectation(description: "wait for save completion")
+        sutToPerfomeSave.save(feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 1.0)
+        
+        let loadExp = expectation(description: "Wait for load completion")
+        sutToPerformLoad.load { loadResult in
+            switch loadResult {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, feed)
+                
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+            }
+            loadExp.fulfill()
+        }
+        wait(for: [loadExp], timeout: 1.0)
+        
     }
 
     //MARK: Helpers
@@ -45,6 +83,18 @@ final class FeedLoaderCacheIntegrationTests: XCTestCase {
 
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 
 }
