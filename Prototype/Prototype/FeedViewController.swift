20b44f89680e98a01f861f0210b174475a202a84
Apostol123 <apostol516@gmail.com>
2023-09-05 07:13:55 +0200

    Add prototype feed data for demoing purpose

---
+++ Prototype/Prototype/FeedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FeedViewController.swift
+//  Prototype
+//
+//  Created by Alex.personal on 4/9/23.
+//
+
+import UIKit
+
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let imageName: String
+}
+
+class FeedViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath)
+    }
+    
+}
