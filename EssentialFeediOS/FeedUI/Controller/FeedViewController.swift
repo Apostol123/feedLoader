138e58d596f43ee6822522d2d044aee222df1ddd
Apostol123 <apostol516@gmail.com>
2023-09-26 07:45:42 +0200

    Remove `EssentailFeed` module import from `FeedViewModel` file since it

--- EssentialFeediOS/FeedUI/Controller/FeedViewController.swift
+++ EssentialFeediOS/FeedUI/Controller/FeedViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import FeedLoader
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     private var refreshController: FeedRefreshViewController?
--- EssentialFeediOS/FeedUI/Views/FeedImageCellController.swift
+++ EssentialFeediOS/FeedUI/Views/FeedImageCellController.swift
@@ -6,50 +6,6 @@
 //
 
 import UIKit
-import FeedLoader
-
-final class FeedImageCellModel {
-    private let imageLoader: FeedImageDataLoader
-    private let model: FeedImage
-    private var tasks: FeedImageDataLoaderTask?
-    
-    init(loader: FeedImageDataLoader, model: FeedImage) {
-        self.imageLoader = loader
-        self.model = model
-    }
-    
-    var location: String? {
-        return model.location
-    }
-    
-    var description: String? {
-        model.description
-    }
-    
-    var imageUrl: URL {
-        return model.url
-    }
-    
-    func preload() {
-        tasks = imageLoader.loadImageData(from: model.url, completion: {_ in })
-    }
-    
-    func cancelLoad() {
-        tasks?.cancel()
-    }
-    
-    func loadImage(completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-        self.tasks = imageLoader.loadImageData(from: model.url) { result in
-            switch result {
-            case .success(let data):
-                completion(.success(data))
-                
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-}
 
 final class FeedImageCellController {
     let cellModel: FeedImageCellModel
