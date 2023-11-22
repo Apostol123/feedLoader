c153e24bff4e63a0718237b4bb1578556e93dd6f
Apostol123 <apostol516@gmail.com>
2023-11-22 07:08:39 +0100

    extract `FeedLoaderStub` into a shared scope to remove duplication

---
+++ EssentialFeed/EssentialFeedTests/Helpers/FeedLoaderStub.swift
@@ -0,0 +1,20 @@
+//
+//  FeedLoaderStub.swift
+//  EssentialFeedTests
+//
+//  Created by Alex.personal on 22/11/23.
+//
+
+import FeedLoader
+
+class FeedLoaderStub: FeedLoader {
+    private let result: FeedLoader.Result
+    
+    init(result: FeedLoader.Result) {
+        self.result = result
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        completion(result)
+    }
+}
