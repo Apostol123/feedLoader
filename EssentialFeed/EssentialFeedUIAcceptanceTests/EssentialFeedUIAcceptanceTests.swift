760883c5be9b5f92924c0dfe3621879da8410df9
Apostol123 <apostol516@gmail.com>
2023-12-02 10:04:48 +0100

    Intercept HTTP request with canned responses druid UI Tests to eliminate network flakiness - We can now run UI tests without internet connection.

--- EssentialFeed/EssentialFeedUIAcceptanceTests/EssentialFeedUIAcceptanceTests.swift
+++ EssentialFeed/EssentialFeedUIAcceptanceTests/EssentialFeedUIAcceptanceTests.swift
@@ -11,18 +11,18 @@ final class EssentialFeedUIAcceptanceTests: XCTestCase {
 
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
-        app.launchArguments = ["-reset"]
+        app.launchArguments = ["-reset", "-connectivity", "online"]
         app.launch()
         
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(feedCells.count, 22)
+        XCTAssertEqual(feedCells.count, 2)
         
         XCTAssertEqual(feedCells.firstMatch.images.count, 1)
     }
     
     func test_onLaunch_displaysCacheRemoteFeedWhenCustomerHasNoConnectivity() {
         let onlineAPP = XCUIApplication()
-        onlineAPP.launchArguments = ["-reset"]
+        onlineAPP.launchArguments = ["-reset", "-connectivity", "online"]
         onlineAPP.launch()
         
         let offlineApp = XCUIApplication()
@@ -30,7 +30,7 @@ final class EssentialFeedUIAcceptanceTests: XCTestCase {
         offlineApp.launch()
         
         let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(cachedFeedCells.count, 22)
+        XCTAssertEqual(cachedFeedCells.count, 2)
         
         XCTAssertEqual(cachedFeedCells.firstMatch.images.count, 1)
     }
