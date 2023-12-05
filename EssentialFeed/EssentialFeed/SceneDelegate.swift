48a5d5c574fcb1e512cdc5a94abc6dcf836df24c
Apostol123 <apostol516@gmail.com>
2023-12-05 16:42:50 +0100

    configures feed navigation as window root view controller

--- EssentialFeed/EssentialFeed/SceneDelegate.swift
+++ EssentialFeed/EssentialFeed/SceneDelegate.swift
@@ -23,6 +23,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        configureWindow()
+    }
+    
+    func configureWindow() {
         let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
         let remoteClient = makeRemoteClient()
         let remoteFeedLoader = RemoteFeedLoader(client: remoteClient, url: url)
@@ -31,7 +35,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
         let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
         let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-        window?.rootViewController = FeedUIComposer.feedComposedWith(
+        window?.rootViewController = UINavigationController(rootViewController:  FeedUIComposer.feedComposedWith(
             loader: FeedLoaderWithFallbackComposite(
                 primary: FeedloaderCacheDecorator(
                     decoratee: remoteFeedLoader,
@@ -41,7 +45,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 primary: FeedImageCacheDecorator(
                     decoratee: remoteImageLoader,
                     cache: localImageLoader),
-                fallback: localImageLoader))
+                fallback: localImageLoader)))
     }
     
     func makeRemoteClient() -> HTTPClient {
