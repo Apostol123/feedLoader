42ad463d4357424d4da45b8533e36dbe52c2f068
Apostol123 <apostol516@gmail.com>
2023-12-09 11:11:55 +0100

    Remove unused debuggind code that was previously used during UI tests

--- EssentialFeed/EssentialFeed/SceneDelegate.swift
+++ EssentialFeed/EssentialFeed/SceneDelegate.swift
@@ -41,7 +41,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func configureWindow() {
         let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
-        let remoteClient = makeRemoteClient()
+        let remoteClient = httpClient
         let remoteFeedLoader = RemoteFeedLoader(client: remoteClient, url: url)
         let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
         
@@ -58,10 +58,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     decoratee: remoteImageLoader,
                     cache: localImageLoader),
                 fallback: localImageLoader)))
-    }
-    
-    func makeRemoteClient() -> HTTPClient {
-        httpClient
     }
 }
 
