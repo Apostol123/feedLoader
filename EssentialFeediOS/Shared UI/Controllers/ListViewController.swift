dee891d03a6709dc1a61d116f7a2e2b4836368b2
Apostol123 <apostol516@gmail.com>
2024-01-11 19:25:46 +0100

    Add default implementation for CellController

--- EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -15,6 +15,11 @@ public protocol CellController {
     func cancelLoad()
 }
 
+public extension CellController {
+    func preload() {}
+    func cancelLoad() {}
+}
+
 
 public final class ListViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView {
     public var onRefresh: (() -> Void)?
