e7335a1c89e0572d7034e2751dadf71307bb97da
Apostol123 <apostol516@gmail.com>
2024-01-13 22:49:36 +0100

    Remove feed references from comments integration tests

--- EssentialFeed/EssentialFeedTests/CommentUIIntegrationTests.swift
+++ EssentialFeed/EssentialFeedTests/CommentUIIntegrationTests.swift
@@ -20,20 +20,7 @@ final class CommentUIIntegrationTests: XCTestCase {
         XCTAssertEqual(sut.title, commentsTitle)
     }
     
-    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
-        let table = "Feed"
-        let bundle = Bundle(for: FeedPresenter.self)
-        let value = bundle.localizedString(forKey: key, value: nil, table: table)
-        if value == key {
-            XCTFail("Missing localized string for key: \(key) in table \(table)", file: file, line: line)
-        }
-        
-        return value
-    }
-    
-   
-    
-    func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+    func test_loadCommentsCompletion_dispatchesFromBackgroundToMainThread() {
         let (sut, loader) = makeCommentsSUT()
         sut.loadViewIfNeeded()
         
